71327bb9e0bf049e456aaa56ae0e387a43287533
Soukaina Ouchen <souchen@e3r1p10.1337.ma>
2022-09-08 14:10:52 +0100

    put pictures

--- script.js
+++ script.js
@@ -2,39 +2,81 @@ const cardArray = [
 
     {
         name: 'fries',
-        img: 'images/fries.jpeg',
+        img: 'images/fries.png',
     },
     {
         name: 'burger',
-        img: 'images/burger.jpeg',
+        img: 'images/burger.png',
     },
     {
         name: 'sandwich',
-        img: 'images/doger.jpeg',
+        img: 'images/doger.png',
     },
     {
         name: 'ice-cream',
-        img: 'images/ice-cream.jpeg',
+        img: 'images/ice-cream.png',
     },
     {
         name: 'pizza',
-        img: 'images/pizza.jpeg',
+        img: 'images/pizza.png',
     },
     {
         name: 'milk',
-        img: 'images/milk.jpg',
+        img: 'images/milk.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'milk',
+        img: 'images/milk.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'burger',
+        img: 'images/burger.png',
+    },
+    {
+        name: 'sandwich',
+        img: 'images/doger.png',
     }
 ]
 
 cardArray.sort(() => 0.5 - Math.random())
 const gridDisplay = document.querySelector('#grid')
+const elementchoose = []
 creatBoard()
 
 function creatBoard() {
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
         const element = document.createElement('img')
-        element.setAttribute('src', 'images/floor3.jpeg')
+        element.setAttribute('src', 'images/floor3.png')
         element.setAttribute('data-id', i)
+        element.addEventListener('click', flipElement)
         gridDisplay.appendChild(element)
+
     }
+}
+
+function check() {
+    if (elementchoose[0] == elementchoose[1])
+        alert('You found a match')
+}
+
+function flipElement() {
+    const elementId = this.getAttribute('data-id')
+    elementchoose.push(cardArray[elementId].name)
+    this.setAttribute('src', cardArray[elementId].img)
+    if (elementchoose.length == 2) {
+        setTimeout(check, 500)
+    }
+
 }
